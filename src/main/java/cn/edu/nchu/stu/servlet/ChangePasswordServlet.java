897d7257f4e378628953a32914704f5c778413bf
曾祥飞 <17204129@stu.nchu.edu.cn>
2019-12-28 21:15:13 +0800

    Changes to be committed: 	new file:   src/main/java/cn/edu/nchu/stu/servlet/ChangePasswordServlet.java 	new file:   web/change_password.jsp

---
+++ src/main/java/cn/edu/nchu/stu/servlet/ChangePasswordServlet.java
@@ -0,0 +1,10 @@
+@javax.servlet.annotation.WebServlet(name = "ChangePasswordServlet")
+public class ChangePasswordServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
--- web/transactions.jsp
+++ web/transactions.jsp
@@ -2,7 +2,7 @@
   Created by IntelliJ IDEA.
   User: zxf
   Date: 2019/12/28
-  Time: 8:05 下午
+  Time: 8:01 下午
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
